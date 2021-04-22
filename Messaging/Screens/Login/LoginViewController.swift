f846b7ae0f37d09e719a8548685f6c56ff28b1ca
Lourdes Dinesh <lourdes@Lourdess-MacBook-Pro.local>
2021-04-22 23:54:58 +0530

    Initiated cocoapods

---
+++ Messaging/Screens/Login/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Messaging
+//
+//  Created by Lourdes on 4/22/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
