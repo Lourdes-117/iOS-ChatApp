1a75ae6d8bb45ec411b1144f8db8816d7f296c0f
Lourdes Dinesh <lourdes@Lourdess-MacBook-Pro.local>
2021-04-22 23:48:13 +0530

    Initial Commit

---
+++ Messaging/Screens/MainScreen/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messaging
+//
+//  Created by Lourdes on 4/22/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
